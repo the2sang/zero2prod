6328991f94b337e65c10d05df2a0721c1ea7f4ef
Charry Kang <the2sang@gmail.com>
2025-01-27 07:27:31 +0900

    4-commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
+use std::fs::Metadata;
+
 pub mod configuration;
 pub mod routes;
 pub mod startup;
 
+pub trait Log: Sync + Send {
+    fn enabled(&self, metadata: &Metadata) -> bool;
+
+   // fn log(&self, record: &Record);
+
+    fn flush(&self);
+}
