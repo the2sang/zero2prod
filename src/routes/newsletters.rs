e4db8601f7b3f2834ad41bbb15ed0c97f50dcf72
Charry Kang <the2sang@gmail.com>
2025-01-30 02:02:10 +0900

    13-commit

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,10 +1,12 @@
 use crate::domain::SubscriberEmail;
 use crate::email_client::EmailClient;
 use crate::routes::error_chain_fmt;
-use actix_web::http::StatusCode;
-use actix_web::{web, HttpResponse, ResponseError};
+use actix_web::http::{StatusCode, header};
+use actix_web::{web, HttpRequest, HttpResponse, ResponseError};
 use anyhow::Context;
+use secrecy::{ExposeSecret, SecretString};
 use sqlx::PgPool;
+use actix_web::http::header::{HeaderMap, HeaderValue };
 
 #[derive(serde::Deserialize)]
 pub struct BodyData {
@@ -20,6 +22,8 @@ pub struct Content {
 
 #[derive(thiserror::Error)]
 pub enum PublishError {
+    #[error("Authentication failed.")]
+    AuthError(#[source] anyhow::Error),
     #[error(transparent)]
     UnexpectedError(#[from] anyhow::Error),
 }
@@ -31,18 +35,54 @@ impl std::fmt::Debug for PublishError {
 }
 
 impl ResponseError for PublishError {
-    fn status_code(&self) -> StatusCode {
+    fn error_response(&self) -> HttpResponse {
         match self {
-            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            PublishError::UnexpectedError(_) => {
+                HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
+            }
+            PublishError::AuthError(_) => {
+                let mut response = HttpResponse::new(StatusCode::UNAUTHORIZED);
+                let header_value = HeaderValue::from_str(r#"Basic realm="publisher""#)
+                    .unwrap();
+                response
+                    .headers_mut().insert(header::WWW_AUTHENTICATE, header_value);
+                response
+            }
         }
     }
+    // fn status_code(&self) -> StatusCode {
+    //     match self {
+    //         PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    //         PublishError::AuthError(_) => StatusCode::UNAUTHORIZED,
+    //     }
+    // }
 }
 
+struct Credentials {
+    username: String,
+    password: SecretString,
+}
+
+#[tracing::instrument(
+    name = "publish a newsletter issue",
+    skip(body,pool, email_client, request),
+    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
 pub async fn publish_newsletter(
     body: web::Json<BodyData>,
     pool: web::Data<PgPool>,
     email_client: web::Data<EmailClient>,
+    request: HttpRequest,
 ) -> Result<HttpResponse, PublishError> {
+    let credentials = basic_authentication(request.headers())
+        .map_err(PublishError::AuthError)?;
+    tracing::Span::current().record(
+        "username",
+        &tracing::field::display(&credentials.username),
+    );
+
+    let user_id = validate_credentials(credentials, &pool).await?;
+
     let subscribers = get_confirmed_subscribers(&pool).await?;
     for subscriber in subscribers {
         match subscriber {
@@ -95,4 +135,62 @@ async fn get_confirmed_subscribers(
         })
         .collect();
     Ok(confirmed_subscribers)
+}
+
+fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Error> {
+    let header_value = headers
+        .get("Authorization")
+        .context("Missing authorization header")?
+        .to_str()
+        .context("Invalid authorization header(was not a valid UTF8 string)")?;
+    let base64encoded_segment = header_value
+        .strip_prefix("Basic ")
+        .context("Invalid authorization header(basic)")?;
+    let decoded_bytes = base64::decode_config(
+        base64encoded_segment,
+        base64::STANDARD
+    )
+        .context("Failed to base64-decode 'Basic' credentials.")?;
+    let decoded_credentials = String::from_utf8(decoded_bytes)
+        .context("Failed to decode 'Basic' credentials.(is not valid UTF8.")?;
+
+
+    let mut credentials = decoded_credentials.splitn(2, ':');
+    let username = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("Missing username in 'Basic' credentials."))?
+        .to_string();
+    let password = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("Missing password in 'Basic' credentials."))?
+        .to_string();
+
+    Ok(Credentials {
+        username,
+        password: SecretString::new(password),
+    })
+}
+
+async fn validate_credentials(
+    credentials: Credentials,
+    pool: &PgPool,
+) -> Result<uuid::Uuid, PublishError> {
+    let user_id: Option<_> = sqlx::query!(
+        r#"
+        SELECT user_id
+        FROM users
+        WHERE username = $1 AND password =  $2
+        "#,
+        credentials.username,
+        credentials.password.expose_secret()
+    )
+        .fetch_optional(pool)
+        .await
+        .context("Failed to perform a query to validate auth credentials.")
+        .map_err(PublishError::UnexpectedError)?;
+
+    user_id
+        .map(|row| row.user_id)
+        .ok_or_else(|| anyhow::anyhow!("Invalied username or password"))
+        .map_err(PublishError::AuthError)
 }
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -34,21 +34,35 @@ pub struct ConfirmationLinks {
 }
 
 impl TestApp {
-    pub async fn post_subscriptions(&self, body: String) -> reqwest::Response {
+    pub async fn post_subscriptions(
+        &self,
+        body: serde_json::Value,
+    ) -> reqwest::Response {
         reqwest::Client::new()
             .post(&format!("{}/subscriptions", &self.address))
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .body(body)
+            .basic_auth(Uuid::new_v4().to_string(), Some(Uuid::new_v4().to_string()))
+            .json(&body)
             .send()
             .await
             .expect("Failed to execute request.")
     }
 
+    pub async fn test_user(&self) -> (String, String) {
+        let row = sqlx::query!("SELECT username, password FROM users LIMIT 1",)
+            .fetch_one(&self.db_pool)
+            .await
+            .expect("Failed to create test user.");
+        (row.username, row.password)
+    }
+
     pub async fn post_newsletters(
         &self,
-        body: serde_json::Value) -> reqwest::Response {
+        body: serde_json::Value,
+    ) -> reqwest::Response {
+        let (username, password) = self.test_user().await;
         reqwest::Client::new()
             .post(&format!("{}/newsletters", &self.address))
+            .basic_auth(username, Some(password))
             .json(&body)
             .send()
             .await
@@ -81,6 +95,9 @@ impl TestApp {
 }
 
 pub async fn spawn_app() -> TestApp {
+
+
+
     LazyLock::force(&TRACING);
 
     // Launch a mock server to stand in for Postmark's API
@@ -98,6 +115,7 @@ pub async fn spawn_app() -> TestApp {
         c
     };
 
+
     // Create and migrate the database
     configure_database(&configuration.database).await;
 
@@ -108,20 +126,35 @@ pub async fn spawn_app() -> TestApp {
     let application_port = application.port();
     let _ = tokio::spawn(application.run_until_stopped());
 
-    TestApp {
+   let test_app =  TestApp {
         address: format!("http://localhost:{}", application_port),
         port: application_port,
         db_pool: get_connection_pool(&configuration.database),
         email_server,
-    }
+    };
+    add_test_user(&test_app.db_pool).await;
+    test_app
+}
+
+
+async fn add_test_user(db_pool: &PgPool) {
+    sqlx::query!(
+        "INSERT INTO users(user_id, username, password) VALUES($1, $2, $3)",
+        Uuid::new_v4(),
+        Uuid::new_v4().to_string(),
+        Uuid::new_v4().to_string(),
+    )
+        .execute(db_pool)
+        .await
+        .expect("Failed to add user to database.");
 }
 
 async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // Create database
     let maintenance_settings = DatabaseSettings {
-        database_name: "postgres".to_string(),
+        database_name: "newsletter".to_string(),
         username: "postgres".to_string(),
-        password: Secret::new("password".to_string()),
+        password: Secret::new("postgres".to_string()),
         ..config.clone()
     };
     let mut connection = PgConnection::connect_with(&maintenance_settings.connect_options())
